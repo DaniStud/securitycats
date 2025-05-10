2bce51a37dc1addc2874e41ee1d1bd20ad0bf1da
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-05-10 18:42:55 +0200

    browsejs-dynamic-url

--- browse.js
+++ browse.js
@@ -1,29 +1,10 @@
+const url = 'http://localhost:5000';
+
 document.body.onload = fetchArticles;
-
-// async function fetchArticles() {
-//     try {
-//         // Fetch articles from the backend
-//         const response = await fetch('http://localhost:5000/get_articles');
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         const result = await response.json();
-
-//         if (result.status === 'success') {
-//             console.log('Articles:', result.data); // Log the articles to the console
-//         } else {
-//             console.error('Error fetching articles:', result.message);
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-
 
 async function fetchArticles() {
     try {
-        // Fetch articles from the backend
-        const response = await fetch('http://localhost:5000/get_articles');
+        const response = await fetch(`${url}/get_articles`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -40,7 +21,7 @@ async function fetchArticles() {
                 <div class="articleWrapper"> 
                 <div class="articleTitle">Title: ${article.title}</div>
                 <div class="articleContent">Content: ${article.article}</div>
-                <a href="http://localhost:5000/${article.article_id}">Security Cats</a>
+                <a href="${url}/${article.article_id}">Security Cats</a>
                 </div>
                 `
 
