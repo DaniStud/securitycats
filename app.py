3755e6be530dc392da27bb005d74cad3df781b48
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-05-11 13:09:17 +0200

    styling-adminroute-added

--- app.py
+++ app.py
@@ -105,6 +105,10 @@ def render_article(article_id):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
